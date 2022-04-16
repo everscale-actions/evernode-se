6c7c45e4f565d692a1c493ce150129fc477c1392
Alexander Salamatov <ch1seL@ya.ru>
2022-04-17 02:01:14 +0300

    fix build

--- nginx/index.js
+++ nginx/index.js
@@ -1,8 +1,8 @@
-import { join } from 'path';
-import { platform } from 'os';
-import { NginxBinary as nginxBinary } from 'nginx-binaries';
+const path = require('path');
+const os = require('os');
+const { NginxBinary: nginxBinary } = require('nginx-binaries');
 
-const nginxBin = platform() === 'win32' ? join('nginx', 'nginx.exe') : join('nginx', 'nginx');
+const nginxBin = os.platform() === 'win32' ? path.join('nginx', 'nginx.exe') : path.join('nginx', 'nginx');
 
 (async () => {
   await nginxBinary.download({ version: process.env.NGINX_VERSION }, nginxBin);
