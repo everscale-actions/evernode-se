dc72bdb52f4b384398fa3123db78978036899c8b
Alexander Salamatov <ch1seL@ya.ru>
2022-11-15 15:23:12 +0300

    add missing q-server vars Q_QUERY_WAIT_FOR_PERIOD and Q_CHAIN_RANGES_VERIFICATION

--- arangodb/config.js
+++ arangodb/config.js
@@ -1,17 +1,19 @@
 export default function getConfig(version) {
-  const basePath = 'https://download.arangodb.com/arangodb39/Community';
-  return {
-    Linux: {
-      url: `${basePath}/Linux/arangodb3-linux-${version}.tar.gz`,
-      pathInArchive: `arangodb3-linux-${version}`,
-    },
-    Windows: {
-      url: `${basePath}/Windows/ArangoDB3-${version}_win64.zip`,
-      pathInArchive: `ArangoDB3-${version}_win64`,
-    },
-    macOS: {
-      url: `${basePath}/MacOSX/arangodb3-macos-${version}.tar.gz`,
-      pathInArchive: `arangodb3-macos-${version}`,
-    },
-  };
+    const ver = version.split('.')
+    const shortVersion = `${ver[0]}${ver[1]}`;
+    const basePath = `https://download.arangodb.com/arangodb${shortVersion}/Community`
+    return {
+        Linux: {
+            url: `${basePath}/Linux/arangodb${ver[0]}-linux-${version}.tar.gz`,
+            pathInArchive: `arangodb3-linux-${version}`,
+        },
+        Windows: {
+            url: `${basePath}/Windows/ArangoDB${ver[0]}-${version}_win64.zip`,
+            pathInArchive: `ArangoDB3-${version}_win64`,
+        },
+        macOS: {
+            url: `${basePath}/MacOSX/arangodb${ver[0]}-macos-${version}.tar.gz`,
+            pathInArchive: `arangodb3-macos-${version}`,
+        },
+    };
 }
